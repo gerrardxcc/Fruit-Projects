bfbabd38d800b9a9101dc4fbf725bb863165ff52
GerrardXiong <chengcheng.xiong@my.jcu.edu.au>
2022-05-09 20:34:29 +1000

    add mongoose and update database

--- app.js
+++ app.js
@@ -1,37 +1,66 @@
-const { MongoClient } = require("mongodb");
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri =
-  "mongodb://localhost:27017";
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    await client.connect();
-    console.log("Connected Successfully to server");
-    const database = client.db('fruitsDB');
-    const fruitsCollection = database.collection('fruits');
+const mongoose = require('mongoose');
 
-    const docs = [
-      { name: "Apple", score:8 , review: "Great fruit"},
-      { name: "Orange", score:6 , review: "Kinda sour"},
-      { name: "Peach", score:9 , review: "Great stuff!"}
-    ];
-    const insertManyresult = await fruitsCollection.insertMany(docs);
-    let ids = insertManyresult.insertedIds;
-    console.log(`${insertManyresult.insertedCount} documents were inserted.`);
-    for (let id of Object.values(ids)) {
-       console.log(`Inserted a document with id ${id}`);
-     }
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-
-  const findResult = await fruits.find({
-  name: "Lemony Snicket",
-  date: {
-    $gte: new Date(new Date().setHours(00, 00, 00)),
-    $lt: new Date(new Date().setHours(23, 59, 59)),
-  },
-});
+// connect to MongoDB by specifying port to access MongoDB server
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/fruitsDB');
 }
-run().catch(console.dir);
+
+// creates new Database if not exist named fruitsDB.
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
+});
+// Creates new collection named Fruits.it pluralize the collection name Fruit to Fruits. and pass the schema.
+
+const Fruit = mongoose.model('Fruit', fruitSchema);
+
+// Insert data to Fruits collection.
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit"
+});
+
+// fruit.save();
+
+//creating new collection People
+const personSchema = mongoose.Schema({
+  name:String,
+  age:Number
+});
+
+const Person = mongoose.model('Person', personSchema);
+const person = new Person({
+  name: "John",
+  age: 37
+});
+
+person.save();
+
+const kiwi = new Fruit({
+  name: "Kiwi",
+  score:10,
+  review:"The best fruit!"
+});
+
+const orange = new Fruit({
+  name: "Orange",
+  score: 4,
+  review:"Too sour for me"
+});
+
+const watermelon = new Fruit({
+  name: "Watermelon",
+  score: 10,
+  review:"The best fruit ever!"
+});
+
+Fruit.insertMany([kiwi,orange,watermelon],function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log("Successfully saved all the fruits to fruitsDB");
+  }
+});
